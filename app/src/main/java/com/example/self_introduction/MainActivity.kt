236b53b954a68d0bfc08c1f7cce4a96e69184243
hiromu00 <hiromu01020@gmail.com>
2023-12-21 15:58:58 +0900

    詳細表示、趣味、住所を記載

--- app/src/main/java/com/example/self_introduction/MainActivity.kt
+++ app/src/main/java/com/example/self_introduction/MainActivity.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.self_introduction.ui.theme.SelfintroductionTheme
@@ -62,6 +64,7 @@ class MainActivity : ComponentActivity() {
                             fontSize = 16.sp,
                             fontWeight = FontWeight.ExtraBold,
                         )
+
                         Spacer(modifier = Modifier.height(20.dp))
 
                         Column(
@@ -73,6 +76,7 @@ class MainActivity : ComponentActivity() {
                                 fontSize = 26.sp,
                                 fontWeight = FontWeight.Bold
                             )
+
                             Spacer(modifier = Modifier.height(20.dp))
 
                             Text(
@@ -82,37 +86,86 @@ class MainActivity : ComponentActivity() {
                             )
                             Spacer(modifier = Modifier.height(20.dp))
 
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Icon(
-                                    imageVector = Icons.Default.Email,
-                                    contentDescription = "email",
-
-
-                                    )
-
-                                Spacer(modifier = Modifier.height(20.dp))
-
-                                Text(
-                                    text = "email",
-                                    fontSize = 14.sp,
-                                    fontWeight = FontWeight.Bold)
-                            }
+                            Label(icon = Icons.Default.Email, text = "email")
 
                             Spacer(modifier = Modifier.height(10.dp))
                             Text(
                                 text = "LOL@ezweb.ne.jp",
                                 fontSize = 16.sp,
-                                fontWeight = FontWeight.Bold)
+                                fontWeight = FontWeight.Bold
+                            )
                             Spacer(modifier = Modifier.height(5.dp))
 
                             Divider(
                                 modifier = Modifier.clip(RoundedCornerShape(1000.dp)),
                                 thickness = 2.dp,
                             )
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                            Button(
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = ButtonDefaults.buttonColors(
+                                    backgroundColor = Color(
+                                        0xFFF85F6A
+                                    )
+                                ),
+                                onClick = { /*TODO*/ },
+                            ) {
+                                Text(
+                                    text = "詳細を表示",
+                                    color = Color.White,
+
+                                    )
+                            }
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clip(RoundedCornerShape(10.dp))
+                                    .background(Color.LightGray.copy(alpha = 0.3f))
+                                    .padding(horizontal = 10.dp, vertical = 20.dp)
+                            ) {
+                                Label(
+                                    icon = Icons.Default.Favorite,
+                                    text = "リーグオブレジェンド",
+                                    color = Color.Gray,
+                                )
+
+                                Spacer(modifier = Modifier.height(10.dp))
+
+                                Label(
+                                    icon = Icons.Default.LocationOn,
+                                    text = "住所：東京都志茂",
+                                    color = Color.Gray,
+                                )
+                            }
                         }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Label(icon: ImageVector, text: String, color: Color = MaterialTheme.colors.onBackground) {
+
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = text,
+            color = color,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
